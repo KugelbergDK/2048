c07a9276579b301bf1d255c2446975d6963af3cf
KugelbergDK <kugelberg.dk@gmail.com>
2019-01-26 01:22:33 +0100

    Added documentation for the Logo + optimize

--- src/sample/Logo.java
+++ src/sample/Logo.java
@@ -9,24 +9,36 @@ import javafx.scene.text.Text;
 
 import static sample.Game2048.CORNER_VALUE;
 
+/**
+ * @author Lucas Kugelberg (Github: github.com/KugelbergDK))
+ *
+ * This class is used to construct the Logo.
+ *
+ */
 public class Logo {
 
     Rectangle logoRect;
 
-    // Construct logo
+    /**
+     * Create a new Rectangle node object with a given size and color.
+     * Add som style to it and add it to the screen.
+     */
     public Logo(){
 
         logoRect = new Rectangle(113, 113, Color.rgb(236, 193, 0));
         logoRect.setArcHeight(CORNER_VALUE);
         logoRect.setArcWidth(CORNER_VALUE);
+        logoRect.setX(15);
+        logoRect.setY(25);
 
         Text logoText = new Text(27, 92, "2048");
         logoText.setFill(Color.WHITE);
         logoText.setStyle("-fx-font: 38px bold; -fx-font-family: 'Arial Rounded MT Bold'");
 
 
+        FXGL.getApp().getGameScene().addUINode(logoRect);
         FXGL.getApp().getGameScene().addUINode(logoText);
-        Entity logo = Entities.builder().at(15,25).viewFromNode(logoRect).buildAndAttach(FXGL.getApp().getGameWorld());
+
 
     }
 }
